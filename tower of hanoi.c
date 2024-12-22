f2bf58f48f82c2cb125c4dd927cf80a6793504a3
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2024-12-22 09:39:05 +0530

    Add files via upload

---
+++ tower of hanoi.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+
+void tower(int n, char from_peg,char aux_peg,char to_peg);
+int main()
+{
+	int n;
+	printf("Enter the number of disks:\n");
+	scanf("%d",&n);
+	tower(n,'A','B','C');
+	printf("Total number of moves= %0.0f\n",pow(2,n)-1);
+	getch();
+}
+
+void tower(int n, char from_peg, char aux_peg,char to_peg)
+{
+	if(n==1)
+	{
+		printf("moves Disk %d from %c peg to %c peg\n",n,from_peg,to_peg);
+		return;
+	}
+	tower(n-1,from_peg,to_peg,aux_peg);
+	printf("Move disk %d from peg %c to %c peg\n",n,from_peg,to_peg);
+	
+	tower(n-1,aux_peg,from_peg,to_peg);
+}
