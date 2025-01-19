3d7109d0cbeff50c51fe4fce32f1f04dcfb773af
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2025-01-19 17:46:42 +0530

    Update practis  dsa.c

--- practis  dsa.c
+++ practis  dsa.c
@@ -1,52 +1,84 @@
 //This page is only for practice the dsa programs its not has specific demonstartation
 
-these programs where not fix here.
 #include<stdio.h>
-#include<conio.h>
-char str[50],pat[20],rep[20],ans[50];
-int c=0;i=0;m=0;j=0,k,flag=0;
-void stringmatch()
+#include<stdlib.h>
+#define MAX 3
+int s[MAX];
+int top=-1;
+
+void push(int item);
+int pop();
+void display();
+
+void push(int item)
 {
-	while(	str[c]!='\0')
+	if(top==MAX-1)
 	{
-		if(str[m]==pat[i])
+		printf("\nStack is overflow\n");
+	}
+	else
+	{
+		top =top+1;
+		s[top]=item;
+	}
+}
+int pop()
+{
+	int item;
+	if(top==-1)
+	{
+		printf("\n Stack if underflow!..");
+	}
+	else
+	{
+		item=s[top];
+		top=top-1;
+		printf("\npoped :%d",item);
+		return item;
+	}
+
+}
+void display()
+{
+	int i;
+	if(top==-1)
+	{
+		printf("\nStack is empty!..");
+	}
+	else{
+		printf("\nStack contents are:");
+		for(i=top;i>=0;i--)
 		{
-			i++;
-			m++;
-			if(pat[i]=='\0')
-			{
-				flag=1;
-				for(k=0;rep[k]!='\0';k++,j++)
-				{
-					ans[j]=rep[k];
-				}
-				i=0;
-				c=m;
-			}
+			printf("\n|%d|",s[i]);
 		}
-		else
+		}	
+}
+
+int main()
+{
+	int ch,item;
+	while(1)
+	{
+		printf("\nMENU");
+		printf("\n1.push");
+		printf("\n2.pop");
+		printf("\n3.display");
+		printf("\n4.exit");
+		printf("\nEnter your choice:");
+		scanf("%d",&ch);
+		switch(ch)
 		{
-			ans[j]=str[c];
-			j++;
-			c++;
-			m=c;
-			i=0;
+			case 1:printf("\nEnter element to be pushed:");
+					scanf("%d",&item);
+					push(item);
+					break;
+			case 2:pop();
+					break;
+			case 3:display();
+					break;
+			case 4:exit(0);
+					break;
+			default:printf("\n please enter valid choice.");								
 		}
 	}
-	ans[i]='\0';
- } 
- 
- int main()
- {
- 	printf("\nEnter the main string: ");
- 	gets(str);
- 	printf("\nEnter the past string: ");
- 	gets(pat);
- 	printf("\nEnter the replace string ");
- 	gets(rep);
- 	stringmatch();
- 	if(flag==1)
- 		printf("\nResultant String is %s ",ans[j]);
- 	else
- 		printf("\nPattern string is not found");
- }
+}
